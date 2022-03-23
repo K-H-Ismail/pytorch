4bb21b347eae6fe5b80609c116ddecb2099878d9
andrewor14 <andrewor14@gmail.com>
2022-03-22 21:31:29 -0700

    [Quant][fx] Define native backend_config_dict for linear and conv
    Summary: This commit changes how quantization patterns for linear
    and conv are set up in prepare. Previously, these were set up
    through ConvReluQuantizeHandler and LinearReLUQuantizeHandler.
    After this commit, however, these were set up through the
    corresponding entries in the native backend_config_dict,
    rendering the above quantize handlers no longer necessary.
    In future commits, we will do the same for the remaining ops.
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx
    python test/test_quantization.py TestQuantizeFxOps
    
    Reviewers: jerryzh168
    
    Subscribers: jerryzh168

--- torch/ao/quantization/fx/backend_config/native.py
+++ torch/ao/quantization/fx/backend_config/native.py
@@ -1,6 +1,10 @@
 import torch
 from .observation_type import ObservationType
 import torch.nn.qat as nnqat
+import torch.nn.intrinsic as nni
+import torch.nn.intrinsic.qat as nniqat
+
+from ...fuser_method_mappings import reverse_sequential_wrapper2
 
 def get_native_backend_config_dict():
     """ Get backend for PyTorch Native backend_config_dict (fbgemm/qnnpack)
@@ -34,11 +38,343 @@ def get_native_backend_config_dict():
         "reference_quantized_module_for_root": torch.nn.quantized._reference.Linear,
         "qat_module": nnqat.Linear,
     }
+    linear_qat_config = {
+        "pattern": nnqat.Linear,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Linear,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Linear,
+    }
+    linear_functional_config = {
+        "pattern": torch.nn.functional.linear,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    linear_relu_fused_config = {
+        "pattern": nni.LinearReLU,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Linear,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Linear,
+        "qat_module": nniqat.LinearReLU,
+    }
+    linear_relu_qat_config = {
+        "pattern": nniqat.LinearReLU,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Linear,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Linear,
+    }
+    linear_relu_mm_config = {
+        "pattern": (torch.nn.ReLU, torch.nn.Linear),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "fuser_method": reverse_sequential_wrapper2(nni.LinearReLU),
+    }
+    linear_relu_mf_config = {
+        "pattern": (torch.nn.functional.relu, torch.nn.Linear),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "fuser_method": reverse_sequential_wrapper2(nni.LinearReLU),
+    }
+    linear_relu_fm_config = {
+        "pattern": (torch.nn.ReLU, torch.nn.functional.linear),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    linear_relu_ff_config = {
+        "pattern": (torch.nn.functional.relu, torch.nn.functional.linear),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    linear_bn_fused_config = {
+        "pattern": nni.LinearBn1d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Linear,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Linear,
+        "qat_module": nniqat.LinearBn1d,
+    }
+    linear_bn_qat_config = {
+        "pattern": nniqat.LinearBn1d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Linear,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Linear,
+    }
 
+    conv1d_module_config = {
+        "pattern": torch.nn.Conv1d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv1d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv1d,
+    }
+    conv2d_module_config = {
+        "pattern": torch.nn.Conv2d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv2d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv2d,
+        "qat_module": nnqat.Conv2d,
+    }
+    conv3d_module_config = {
+        "pattern": torch.nn.Conv3d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv3d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv3d,
+        "qat_module": nnqat.Conv3d,
+    }
+    # TODO: add support for qat.Conv1d
+    conv2d_qat_config = {
+        "pattern": nnqat.Conv2d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv2d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv2d,
+    }
+    conv3d_qat_config = {
+        "pattern": nnqat.Conv3d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv3d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv3d,
+    }
+    conv1d_functional_config = {
+        "pattern": torch.nn.functional.conv1d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv2d_functional_config = {
+        "pattern": torch.nn.functional.conv2d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv3d_functional_config = {
+        "pattern": torch.nn.functional.conv3d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv1d_relu_fused_config = {
+        "pattern": nni.ConvReLU1d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv1d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv1d,
+    }
+    conv2d_relu_fused_config = {
+        "pattern": nni.ConvReLU2d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv2d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv2d,
+        "qat_module": nniqat.ConvReLU2d,
+    }
+    conv3d_relu_fused_config = {
+        "pattern": nni.ConvReLU3d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv2d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv3d,
+        "qat_module": nniqat.ConvReLU3d,
+    }
+    # TODO: add support for qat.ConvReLU1d
+    conv2d_relu_qat_config = {
+        "pattern": nniqat.ConvReLU2d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv2d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv2d,
+    }
+    conv3d_relu_qat_config = {
+        "pattern": nniqat.ConvReLU3d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv3d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv3d,
+    }
+    conv1d_relu_fm_config = {
+        "pattern": (torch.nn.ReLU, torch.nn.functional.conv3d),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv2d_relu_fm_config = {
+        "pattern": (torch.nn.ReLU, torch.nn.functional.conv3d),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv3d_relu_fm_config = {
+        "pattern": (torch.nn.ReLU, torch.nn.functional.conv3d),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv1d_relu_ff_config = {
+        "pattern": (torch.nn.functional.relu, torch.nn.functional.conv1d),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv2d_relu_ff_config = {
+        "pattern": (torch.nn.functional.relu, torch.nn.functional.conv2d),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv3d_relu_ff_config = {
+        "pattern": (torch.nn.functional.relu, torch.nn.functional.conv3d),
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+    }
+    conv1d_bn_qat_config = {
+        "pattern": nniqat.ConvBn1d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv1d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv1d,
+    }
+    conv2d_bn_qat_config = {
+        "pattern": nniqat.ConvBn2d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv2d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv2d,
+    }
+    conv3d_bn_qat_config = {
+        "pattern": nniqat.ConvBn3d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv3d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv3d,
+    }
+    conv1d_bn_relu_qat_config = {
+        "pattern": nniqat.ConvBnReLU1d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv1d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv1d,
+    }
+    conv2d_bn_relu_qat_config = {
+        "pattern": nniqat.ConvBnReLU2d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv2d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv2d,
+    }
+    conv3d_bn_relu_qat_config = {
+        "pattern": nniqat.ConvBnReLU3d,
+        "observation_type": ObservationType.OUTPUT_USE_DIFFERENT_OBSERVER_AS_INPUT,
+        "dtype_configs": [
+            weighted_op_int8_dtype_config,
+        ],
+        "root_module": torch.nn.Conv3d,
+        "reference_quantized_module_for_root": torch.nn.quantized._reference.Conv3d,
+    }
     return {
         # optional
         "name": "native",
         "configs": [
             linear_module_config,
+            linear_qat_config,
+            linear_functional_config,
+            linear_relu_fused_config,
+            linear_relu_qat_config,
+            linear_relu_mm_config,
+            linear_relu_mf_config,
+            #linear_relu_fm_config,
+            #linear_relu_ff_config,
+            linear_bn_fused_config,
+            linear_bn_qat_config,
+            conv1d_module_config,
+            conv2d_module_config,
+            conv3d_module_config,
+            conv2d_qat_config,
+            conv3d_qat_config,
+            conv1d_functional_config,
+            conv2d_functional_config,
+            conv3d_functional_config,
+            conv1d_relu_fused_config,
+            conv2d_relu_fused_config,
+            conv3d_relu_fused_config,
+            conv2d_relu_qat_config,
+            conv3d_relu_qat_config,
+            #conv1d_relu_fm_config,
+            #conv2d_relu_fm_config,
+            #conv3d_relu_fm_config,
+            #conv1d_relu_ff_config,
+            #conv2d_relu_ff_config,
+            #conv3d_relu_ff_config,
+            conv1d_bn_qat_config,
+            conv2d_bn_qat_config,
+            conv3d_bn_qat_config,
+            conv1d_bn_relu_qat_config,
+            conv2d_bn_relu_qat_config,
+            conv3d_bn_relu_qat_config,
         ],
     }
